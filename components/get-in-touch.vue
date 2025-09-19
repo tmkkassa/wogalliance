2db76bfdc2175475b0c1c560272cb73c6a962427
tmk kassa <tmkkassa@gmail.com>
2025-09-19 01:59:46 -0400

    final touches

--- components/get-in-touch.vue
+++ components/get-in-touch.vue
@@ -4,7 +4,7 @@
             touch!</h3>
         <p class="text-slate-400 max-w-xl mx-auto">If you have any question please don't hesitate to contact us.</p>
         <div class="mt-6">
-            <NuxtLink to="/contact"
+            <NuxtLink to="/contactus"
                 class="h-10 px-5 tracking-wide inline-flex items-center justify-center font-medium rounded-md bg-primary text-white hover:bg-blue-700 transition-colors duration-300 shadow-lg hover:shadow-xl">
                 <i class="iconoir-phone align-middle me-2"></i> Contact us
             </NuxtLink>
